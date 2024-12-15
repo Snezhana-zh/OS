b052eb13e1c9cb9a31e7a4fc1a269aaa67aad22e
Снежана Жуйко <67885606+Snezhana-zh@users.noreply.github.com>
2024-12-15 13:30:29 +0700

    fix cache sync

--- cache.cpp
+++ cache.cpp
@@ -1,40 +1,69 @@
 #include "cache.h"
 
-CacheItem::CacheItem(std::string k, std::string d) : key(k), data(d) {
-    use_count = 1;
+CacheItem::CacheItem(std::string k, std::string d) : key(k), data(d), createdTime(std::chrono::steady_clock::now()) {
 }
 
 std::string& CacheItem::getData() {
     return data;
 }
 
-std::string& CacheItem::getKey() {
-    return key;
-}
+bool CacheItem::timeOut(std::chrono::time_point<std::chrono::steady_clock> time) {
+    auto elapsed = std::chrono::duration_cast<std::chrono::minutes>(time - createdTime);
 
-int CacheItem::getUseCount() {
-    return use_count;
+    if (elapsed >= ttl) {
+        return true;
+    }
+    return false;
 }
 
 Cache::Cache() {
-    pthread_mutexattr_t attr;
-    pthread_mutexattr_init(&attr);
-    pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_PRIVATE);
+    // pthread_mutexattr_t attr;
+    // pthread_mutexattr_init(&attr);
+    // pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_PRIVATE);
 
-    int err = pthread_mutex_init(&mutex, &attr);
-	pthread_mutexattr_destroy(&attr);
+    // int err = pthread_mutex_init(&lock, &attr);
+	// pthread_mutexattr_destroy(&attr);
+    // if (err) {
+    //     throw std::runtime_error("pthread_mutex_init() failed");
+    // }
+    pthread_rwlockattr_t attr;
+    int err;
+
+    err = pthread_rwlockattr_init(&attr);
+    if (err) throw std::runtime_error("pthread_rwlockattr_init() failed");
+
+    err = pthread_rwlockattr_setpshared(&attr, PTHREAD_PROCESS_PRIVATE);
+    if (err) throw std::runtime_error("pthread_rwlockattr_setpshared() failed");
+
+    err = pthread_rwlock_init(&lock, &attr);
     if (err) {
-        throw std::runtime_error("pthread_mutex_init() failed");
+        throw std::runtime_error("pthread_rwlock_init() failed");
+    }
+}
+
+void Cache::clean() {
+    if (cache_map.size() > QUEUE_SIZE) {
+        for (auto it = cache_map.begin(); it != cache_map.end();) {
+            if (it->second->timeOut(std::chrono::steady_clock::now())) {
+                it = cache_map.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+    }
+
+    if (cache_map.size() > QUEUE_SIZE) {
+        auto it = cache_map.begin();
+        
+        cache_map.erase(it);
     }
 }
 
 void Cache::put(std::string key, std::string data) {
-    pthread_mutex_lock(&mutex);
+    pthread_rwlock_wrlock(&lock);
 
-    if (cache_map.size() > QUEUE_SIZE) {
-        auto it = cache_map.begin();
-        cache_map.erase(it);
-    }
+    clean();
 
     auto it = cache_map.find(key);
 
@@ -42,12 +71,15 @@ void Cache::put(std::string key, std::string data) {
         CacheItem* item = new CacheItem(key, data);
         cache_map[key] = item;
     }
+    else {
+        it->second->getData() = data;
+    }
 
-    pthread_mutex_unlock(&mutex);
+    pthread_rwlock_unlock(&lock);
 }
 
 std::string Cache::get(std::string key) {
-    pthread_mutex_lock(&mutex);
+    pthread_rwlock_rdlock(&lock);
 
     std::string data = "";
 
@@ -57,7 +89,7 @@ std::string Cache::get(std::string key) {
         data = it->second->getData();
     }
 
-    pthread_mutex_unlock(&mutex);
+    pthread_rwlock_unlock(&lock);
     return data;
 }
 
@@ -69,15 +101,19 @@ std::pair<std::string, bool> Cache::hasElem(std::string key) {
 }
 
 Cache::~Cache() {
-    int err = pthread_mutex_destroy(&mutex);
-
-    if (err) {
-        std::cerr << "pthread_mutex_destroy() failed: " << std::endl;
-    }
+    pthread_rwlock_wrlock(&lock);
 
     for (auto& elem : cache_map) {
         delete elem.second;
     }
     
     cache_map.clear();
+
+    pthread_rwlock_unlock(&lock);
+
+    int err = pthread_rwlock_destroy(&lock);
+
+    if (err) {
+        std::cerr << "pthread_rwlock_destroy() failed: " << std::endl;
+    } 
 }
--- cache.h
+++ cache.h
@@ -9,19 +9,20 @@
 #include <errno.h>
 #include <list>
 #include <queue>
+#include <chrono>
 
 #define QUEUE_SIZE 64
 
 class CacheItem {
 private:
-    int use_count;
     std::string data;
     std::string key;
+    std::chrono::time_point<std::chrono::steady_clock> createdTime;
+    std::chrono::minutes ttl{1};
 public:
     CacheItem(std::string key, std::string data);
     std::string& getData();
-    std::string& getKey();
-    int getUseCount();
+    bool timeOut(std::chrono::time_point<std::chrono::steady_clock> time);
 };
 
 class Cache {
@@ -34,11 +35,15 @@ public:
 
     std::pair<std::string, bool> hasElem(std::string key);
 
+    void clean();
+
     ~Cache();
 private:
     std::map<std::string, CacheItem*> cache_map;
 
-    pthread_mutex_t mutex;
+    pthread_rwlock_t lock; // erase don't thread safety
+
+    // pthread_mutex_t mutex;
 };
 
 #endif
